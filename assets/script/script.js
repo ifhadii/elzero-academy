686125c68cde9815c68390df8d533d526c2cd8df
ifhadii <xfahadmakeenx@gmail.com>
2024-10-26 23:47:02 +0300

    pushing

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,6 +3,11 @@ window.onscroll = function() {
     if (window.scrollY > 50) { // Adjust the scroll distance as needed
         header.style.backgroundColor = "#010101a6";
     } else {
-        header.style.backgroundColor = "transparent"; // Original color or transparent
+        // Original color or transparent
+        header.style.backgroundColor = "transparent";
     }
 };
+
+function main() {
+   // will see 
+}
