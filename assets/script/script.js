da738c93c23d89747389390558747002cd785225
ifhadii <xfahadmakeenx@gmail.com>
2024-10-31 21:44:15 +0300

    pushing

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,9 +1,19 @@
-// fun change header color on scroll
 window.onscroll = function() {
-    var header = document.querySelector('.header');
+    let header = document.querySelector(".header");
     if (window.scrollY > 50) { 
         header.style.backgroundColor = "#010101a6";
     } else {
         header.style.backgroundColor = "transparent";
     }
 };
+
+
+function toggleDisplayMenu() {
+    let menu = document.querySelector(".header .container .menu .links-header ul");
+
+    if (menu.style.display === "none" || menu.style.display === "") {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
+}
