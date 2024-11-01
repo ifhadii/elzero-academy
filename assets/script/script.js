6d9b35f08ee78e4a71074d46440c61dd9b9865fd
ifhadii <xfahadmakeenx@gmail.com>
2024-11-02 01:59:44 +0300

    pushing

--- assets/script/script.js
+++ assets/script/script.js
@@ -8,12 +8,32 @@ window.onscroll = function() {
 };
 
 
-function toggleDisplayMenu() {
-    let menu = document.querySelector(".header .container .menu .links-header ul");
+// function toggleDisplayMenu() {
+//     let menu = document.querySelector(".header .container .menu .links-header ul");
 
-    if (menu.style.display === "none" || menu.style.display === "") {
-        menu.style.display = "block";
-    } else {
-        menu.style.display = "none";
-    }
-}
+//     if (menu.style.display === "block" || menu.style.display === "") {
+//         menu.style.display = "block";
+//     } else {
+//         menu.style.display = "none";
+//     }
+// }
+
+// // Function to handle screen resize and adjust menu layout
+// function handleResize() {
+//     let menu = document.querySelector(".header .container .menu .links-header ul");
+//     if (window.innerWidth > 992) {
+//         // Reset to horizontal layout on wide screen
+//         menu.style.display = "flex";
+//         menu.style.flexDirection = "row";
+//     } else {
+//         // Set to vertical layout on smaller screens
+//         menu.style.flexDirection = "column";
+//     }
+// }
+
+// // Attach resize event listener to adjust layout on screen change
+// window.addEventListener("resize", handleResize);
+
+// // Initialize layout based on current screen size
+// handleResize();
+
