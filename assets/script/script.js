522b8960ffd23c496c6857e126d60a7b25c2eba3
ifhadii <xfahadmakeenx@gmail.com>
2024-11-04 05:52:32 +0300

    pushing

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,6 @@
-window.onscroll = function() {
+window.onscroll = function () {
     let header = document.querySelector(".header");
-    if (window.scrollY > 50) { 
+    if (window.scrollY > 50) {
         header.style.backgroundColor = "#010101a6";
     } else {
         header.style.backgroundColor = "transparent";
@@ -37,3 +37,21 @@ window.onscroll = function() {
 // // Initialize layout based on current screen size
 // handleResize();
 
+
+// thin card styleiztion
+const techElements = document.getElementsByClassName("technology");
+
+for (let i = 0; i < techElements.length; i++) {
+    const techName = techElements[i].innerText;
+
+    if (techName === "Wordpress") {
+        techElements[i].style.backgroundColor = "#d54e21";
+    } else if (techName === "Vuejs") {
+        techElements[i].style.backgroundColor = "#42b883";
+    } else if (techName === "Javascript") {
+        techElements[i].style.backgroundColor = "#f7df1e";
+        techElements[i].style.color = "#333";
+    } else if (techName === "Programming") {
+        techElements[i].style.backgroundColor = "#7552cc";
+    }
+}
